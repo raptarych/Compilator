400814f3450b3b8b8abe26760e7d9463038157cd
Араптанов Артём <artem_a@holdingbp.ru>
2017-06-02 13:42:36 +0500

    Ещё чуть рефакторнул

--- Compilator/CommonTables.cs
+++ Compilator/CommonTables.cs
@@ -2,34 +2,6 @@
 
 namespace Compilator
 {
-    /// <summary>
-    /// Сущность лексемы; содержит тип лексемы и ссылку на своё значение
-    /// </summary>
-    public class Lexem
-    {
-        public LexemType Key { get; set; }
-        public byte ValuePtr { get; set; }
-
-        public object GetValue()
-        {
-            switch (Key)
-            {
-                case LexemType.CONSTANT:
-                    return CommonTables.Constants[ValuePtr];
-                case LexemType.KEYWORD:
-                    return CommonTables.Keywords[ValuePtr];
-                case LexemType.IDENTIFIER:
-                    return CommonTables.Identifiers[ValuePtr];
-                case LexemType.OPERATION:
-                    return CommonTables.Operations[ValuePtr];
-                default:
-                    return (char) ValuePtr;
-            }
-                
-        }
-
-    }
-
     static class CommonTables
     {
         public static Dictionary<string, object> Variables = new Dictionary<string, object>();
---
+++ Compilator/Lexem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compilator
+{
+    /// <summary>
+    /// Сущность лексемы; содержит тип лексемы и ссылку на своё значение
+    /// </summary>
+    public class Lexem
+    {
+        public LexemType Key { get; set; }
+        public byte ValuePtr { get; set; }
+
+        public object GetValue()
+        {
+            switch (Key)
+            {
+                case LexemType.CONSTANT:
+                    return CommonTables.Constants[ValuePtr];
+                case LexemType.KEYWORD:
+                    return CommonTables.Keywords[ValuePtr];
+                case LexemType.IDENTIFIER:
+                    return CommonTables.Identifiers[ValuePtr];
+                case LexemType.OPERATION:
+                    return CommonTables.Operations[ValuePtr];
+                default:
+                    return (char)ValuePtr;
+            }
+        }
+    }
+}
